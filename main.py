3f918bae2fd04a96c5e98a34f09900802ad6bbc5
Jeison Verjan <109113104+jeisonverjan@users.noreply.github.com>
2022-08-19 17:03:14 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == __name__:
     exit = ''
     while exit != 'x':
         show_questions()
-        print('press "X" to exit, other key to continue')
+        print('press "X" to exit, another key to continue')
         exit = input().strip().lower()
